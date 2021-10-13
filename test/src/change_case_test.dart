3baee4323fbc19675aab5119cd8a145623e307ed
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-13 18:33:02 -0500

    feat: add tests

--- test/src/change_case_test.dart
+++ test/src/change_case_test.dart
@@ -2,215 +2,317 @@ import 'package:change_case/change_case.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('#resetToCamelCase', () {
-    test('return empty string if provided empty string', () {
-      expect(resetToCamelCase(''), '');
-    });
-
-    group('pascal', () {
-      CaseTester.runResetToCamelTests(
-        input: 'PascalCase',
-        expected: 'pascalCase',
+  group('#single word', () {
+    group('all lower case', () {
+      ChangeCaseTester.runTests(
+        input: 'hello',
+        toCamelExpect: 'hello',
+        toCapitalExpect: 'Hello',
+        toConstantExpect: 'HELLO',
+        toDotExpect: 'hello',
+        toHeaderExpect: 'Hello',
+        toKebabExpect: 'hello',
+        toParamExpect: 'hello',
+        toLowerFirstExpect: 'hello',
+        toNoExpect: 'hello',
+        toPascalExpect: 'Hello',
+        toPathExpect: 'hello',
+        toSentenceExpect: 'Hello',
+        toSnakeExpect: 'hello',
+        toSwapExpect: 'HELLO',
+        toTitleExpect: 'Hello',
+        toUpperFirstExpect: 'Hello',
       );
     });
 
-    group('camel', () {
-      CaseTester.runResetToCamelTests(
-        input: 'camelCase',
-        expected: 'camelCase',
+    group('all upper case', () {
+      ChangeCaseTester.runTests(
+        input: 'HELLO',
+        toCamelExpect: 'hello',
+        toCapitalExpect: 'Hello',
+        toConstantExpect: 'HELLO',
+        toDotExpect: 'HELLO',
+        toHeaderExpect: 'Hello',
+        toKebabExpect: 'hello',
+        toParamExpect: 'hello',
+        toLowerFirstExpect: 'hELLO',
+        toNoExpect: 'hello',
+        toPascalExpect: 'Hello',
+        toPathExpect: 'hello',
+        toSentenceExpect: 'Hello',
+        toSnakeExpect: 'hello',
+        toSwapExpect: 'hello',
+        toTitleExpect: 'HELLO',
+        toUpperFirstExpect: 'HELLO',
       );
     });
 
-    group('snake', () {
-      CaseTester.runResetToCamelTests(
-        input: 'snake_case',
-        expected: 'snakeCase',
+    group('first letter capitalized', () {
+      ChangeCaseTester.runTests(
+        input: 'Hello',
+        toCamelExpect: 'hello',
+        toCapitalExpect: 'Hello',
+        toConstantExpect: 'HELLO',
+        toDotExpect: 'Hello',
+        toHeaderExpect: 'Hello',
+        toKebabExpect: 'hello',
+        toParamExpect: 'hello',
+        toLowerFirstExpect: 'hello',
+        toNoExpect: 'hello',
+        toPascalExpect: 'Hello',
+        toPathExpect: 'hello',
+        toSentenceExpect: 'Hello',
+        toSnakeExpect: 'hello',
+        toSwapExpect: 'hELLO',
+        toTitleExpect: 'Hello',
+        toUpperFirstExpect: 'Hello',
       );
     });
 
-    group('kebab', () {
-      CaseTester.runResetToCamelTests(
-        input: 'kebab-case',
-        expected: 'kebabCase',
+    group('last letter capitalized', () {
+      ChangeCaseTester.runTests(
+        input: 'hellO',
+        toCamelExpect: 'hellO',
+        toCapitalExpect: 'Hell O',
+        toConstantExpect: 'HELL_O',
+        toDotExpect: 'hell.O',
+        toHeaderExpect: 'Hell-O',
+        toKebabExpect: 'hell-o',
+        toParamExpect: 'hell-o',
+        toLowerFirstExpect: 'hellO',
+        toNoExpect: 'hell o',
+        toPascalExpect: 'HellO',
+        toPathExpect: 'hell/o',
+        toSentenceExpect: 'Hell o',
+        toSnakeExpect: 'hell_o',
+        toSwapExpect: 'HELLo',
+        toTitleExpect: 'hellO',
+        toUpperFirstExpect: 'HellO',
       );
     });
 
-    group('title', () {
-      CaseTester.runResetToCamelTests(
-        input: 'Title Case',
-        expected: 'titleCase',
-      );
-    });
-
-    group('pascal-snake', () {
-      CaseTester.runResetToCamelTests(
-        input: 'Pascal_Case',
-        expected: 'pascalCase',
-      );
-    });
-
-    group('pascal-kebab', () {
-      CaseTester.runResetToCamelTests(
-        input: 'Pascal-Case',
-        expected: 'pascalCase',
-      );
-    });
-
-    group('pascal-kebab-snake', () {
-      CaseTester.runResetToCamelTests(
-        input: 'Pascal-Kebab_Case',
-        expected: 'pascalKebabCase',
+    group('has numbers', () {
+      ChangeCaseTester.runTests(
+        input: 'hello123',
+        toCamelExpect: 'hello123',
+        toCapitalExpect: 'Hello123',
+        toConstantExpect: 'HELLO123',
+        toDotExpect: 'hello123',
+        toHeaderExpect: 'Hello123',
+        toKebabExpect: 'hello123',
+        toParamExpect: 'hello123',
+        toLowerFirstExpect: 'hello123',
+        toNoExpect: 'hello123',
+        toPascalExpect: 'Hello123',
+        toPathExpect: 'hello123',
+        toSentenceExpect: 'Hello123',
+        toSnakeExpect: 'hello123',
+        toSwapExpect: 'HELLO123',
+        toTitleExpect: 'Hello123',
+        toUpperFirstExpect: 'Hello123',
       );
     });
   });
 
-  group('CaseHelper', () {
-    // const pascalWord = 'FormatWord';
-    // const snakeWord = 'snake_word';
-    // const camelWord = 'camelWord';
-    // const kebabWord = 'kebab-word';
-    // const pascalWordWithNumbers = 'Pascal123';
-    // const snakeWordWithNumbers = 'snake_word123';
-    // const camelWordWithNumbers = 'camelWord123';
-    // const kebabWordWithNumbers = 'kebab-word123';
-
-    group('pascal', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'PascalWord',
-        toPascalExpected: 'PascalWord',
-        toSnakeExpected: 'pascal_word',
-        toCamelExpected: 'pascalWord',
-        toKebabExpected: 'pascal-word',
+  group('multiple words', () {
+    group('single space inbetween', () {
+      ChangeCaseTester.runTests(
+        input: 'hello world',
+        toCamelExpect: 'helloWorld',
+        toCapitalExpect: 'Hello World',
+        toConstantExpect: 'HELLO_WORLD',
+        toDotExpect: 'hello.world',
+        toHeaderExpect: 'Hello-World',
+        toKebabExpect: 'hello-world',
+        toParamExpect: 'hello-world',
+        toLowerFirstExpect: 'hello world',
+        toNoExpect: 'hello world',
+        toPascalExpect: 'HelloWorld',
+        toPathExpect: 'hello/world',
+        toSentenceExpect: 'Hello world',
+        toSnakeExpect: 'hello_world',
+        toSwapExpect: 'HELLO WORLD',
+        toTitleExpect: 'Hello World',
+        toUpperFirstExpect: 'Hello world',
       );
     });
 
-    group('snake', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'snake_word',
-        toPascalExpected: 'SnakeWord',
-        toSnakeExpected: 'snake_word',
-        toCamelExpected: 'snakeWord',
-        toKebabExpected: 'snake-word',
+    group('multiple spaces inbetween', () {
+      ChangeCaseTester.runTests(
+        input: 'hello   world',
+        toCamelExpect: 'helloWorld',
+        toCapitalExpect: 'Hello World',
+        toConstantExpect: 'HELLO_WORLD',
+        toDotExpect: 'hello.world',
+        toHeaderExpect: 'Hello-World',
+        toKebabExpect: 'hello-world',
+        toParamExpect: 'hello-world',
+        toLowerFirstExpect: 'hello   world',
+        toNoExpect: 'hello world',
+        toPascalExpect: 'HelloWorld',
+        toPathExpect: 'hello/world',
+        toSentenceExpect: 'Hello world',
+        toSnakeExpect: 'hello_world',
+        toSwapExpect: 'HELLO   WORLD',
+        toTitleExpect: 'Hello   World',
+        toUpperFirstExpect: 'Hello   world',
       );
     });
 
-    group('camel', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'camelWord',
-        toPascalExpected: 'CamelWord',
-        toSnakeExpected: 'camel_word',
-        toCamelExpected: 'camelWord',
-        toKebabExpected: 'camel-word',
+    group('trailing & leading space', () {
+      ChangeCaseTester.runTests(
+        input: ' hello world ',
+        toCamelExpect: 'helloWorld',
+        toCapitalExpect: ' Hello World ',
+        toConstantExpect: '_HELLO_WORLD_',
+        toDotExpect: '.hello.world.',
+        toHeaderExpect: '-Hello-World-',
+        toKebabExpect: '-hello-world-',
+        toParamExpect: '-hello-world-',
+        toLowerFirstExpect: ' hello world ',
+        toNoExpect: ' hello world ',
+        toPascalExpect: 'HelloWorld',
+        toPathExpect: '/hello/world/',
+        toSentenceExpect: 'Hello world',
+        toSnakeExpect: '_hello_world_',
+        toSwapExpect: ' HELLO WORLD ',
+        toTitleExpect: ' Hello World ',
+        toUpperFirstExpect: 'Hello world ',
+      );
+    });
+  });
+
+  group('numbers', () {
+    group('leading', () {
+      ChangeCaseTester.runTests(
+        input: '123hello',
+        toCamelExpect: '123hello',
+        toCapitalExpect: '123hello',
+        toConstantExpect: '123HELLO',
+        toDotExpect: '123hello',
+        toHeaderExpect: '123hello',
+        toKebabExpect: '123hello',
+        toParamExpect: '123hello',
+        toLowerFirstExpect: '123hello',
+        toNoExpect: '123hello',
+        toPascalExpect: '123hello',
+        toPathExpect: '123hello',
+        toSentenceExpect: '123hello',
+        toSnakeExpect: '123hello',
+        toSwapExpect: '123HELLO',
+        toTitleExpect: '123hello',
+        toUpperFirstExpect: '123hello',
       );
     });
 
-    group('kebab', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'kebab-word',
-        toPascalExpected: 'KebabWord',
-        toSnakeExpected: 'kebab_word',
-        toCamelExpected: 'kebabWord',
-        toKebabExpected: 'kebab-word',
-      );
-    });
-
-    group('pascal-snake', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'Pascal_Word',
-        toPascalExpected: 'PascalWord',
-        toSnakeExpected: 'pascal_word',
-        toCamelExpected: 'pascalWord',
-        toKebabExpected: 'pascal-word',
-      );
-    });
-
-    group('pascal-kebab', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'Pascal-Word',
-        toPascalExpected: 'PascalWord',
-        toSnakeExpected: 'pascal_word',
-        toCamelExpected: 'pascalWord',
-        toKebabExpected: 'pascal-word',
-      );
-    });
-
-    group('pascal-kebab-snake', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'Pascal-Kebab_Word',
-        toPascalExpected: 'PascalKebabWord',
-        toSnakeExpected: 'pascal_kebab_word',
-        toCamelExpected: 'pascalKebabWord',
-        toKebabExpected: 'pascal-kebab-word',
-      );
-    });
-
-    group('pascal-kebab-snake-numbers', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'Pascal-Kebab_Word123',
-        toPascalExpected: 'PascalKebabWord123',
-        toSnakeExpected: 'pascal_kebab_word123',
-        toCamelExpected: 'pascalKebabWord123',
-        toKebabExpected: 'pascal-kebab-word123',
-      );
-    });
-
-    group('pascal-kebab-snake-numbers-pascal', () {
-      CaseTester.runFormatExtensionTests(
-        input: 'Pascal-Kebab_Word123Pascal',
-        toPascalExpected: 'PascalKebabWord123Pascal',
-        toSnakeExpected: 'pascal_kebab_word123_pascal',
-        toCamelExpected: 'pascalKebabWord123Pascal',
-        toKebabExpected: 'pascal-kebab-word123-pascal',
+    group('trailing', () {
+      ChangeCaseTester.runTests(
+        input: 'hello123',
+        toCamelExpect: 'hello123',
+        toCapitalExpect: 'Hello123',
+        toConstantExpect: 'HELLO123',
+        toDotExpect: 'hello123',
+        toHeaderExpect: 'Hello123',
+        toKebabExpect: 'hello123',
+        toParamExpect: 'hello123',
+        toLowerFirstExpect: 'hello123',
+        toNoExpect: 'hello123',
+        toPascalExpect: 'Hello123',
+        toPathExpect: 'hello123',
+        toSentenceExpect: 'Hello123',
+        toSnakeExpect: 'hello123',
+        toSwapExpect: 'HELLO123',
+        toTitleExpect: 'Hello123',
+        toUpperFirstExpect: 'Hello123',
       );
     });
   });
 }
 
-class CaseTester {
-  const CaseTester._();
+class ChangeCaseTester {
+  const ChangeCaseTester._();
 
-  static void runFormatExtensionTests({
+  static void runTests({
     required String input,
-    required String toPascalExpected,
-    required String toSnakeExpected,
-    required String toCamelExpected,
-    required String toKebabExpected,
+    required String toCamelExpect,
+    required String toCapitalExpect,
+    required String toConstantExpect,
+    required String toDotExpect,
+    required String toHeaderExpect,
+    required String toKebabExpect,
+    required String toParamExpect,
+    required String toLowerFirstExpect,
+    required String toNoExpect,
+    required String toPascalExpect,
+    required String toPathExpect,
+    required String toSentenceExpect,
+    required String toSnakeExpect,
+    required String toSwapExpect,
+    required String toTitleExpect,
+    required String toUpperFirstExpect,
   }) {
-    // test('to pascal', () {
-    //   expect(input.pascal, toPascalExpected);
-    // });
-
-    // test('to snake', () {
-    //   expect(input.snake, toSnakeExpected);
-    // });
-
-    // test('to camel', () {
-    //   expect(input.camel, toCamelExpected);
-    // });
-
-    // test('to kebab', () {
-    //   expect(input.kebab, toKebabExpected);
-    // });
-  }
-
-  static void runResetToCamelTests({
-    required String input,
-    required String expected,
-  }) {
-    test('returns camel', () {
-      expect(resetToCamelCase(input), expected);
+    test('toCamelCase', () {
+      expect(input.toCamelCase(), toCamelExpect);
     });
 
-    test('returns camel with leading and trailing white space', () {
-      expect(resetToCamelCase('  $input  '), expected);
+    test('toCapitalCase', () {
+      expect(input.toCapitalCase(), toCapitalExpect);
     });
 
-    test('returns camel with numbers starting', () {
-      expect(resetToCamelCase('123$input'), '123$expected');
+    test('toConstantCase', () {
+      expect(input.toConstantCase(), toConstantExpect);
     });
 
-    test('returns camel with numbers ending', () {
-      expect(resetToCamelCase('${input}123'), '${expected}123');
+    test('toDotCase', () {
+      expect(input.toDotCase(), toDotExpect);
+    });
+
+    test('toHeaderCase', () {
+      expect(input.toHeaderCase(), toHeaderExpect);
+    });
+
+    test('toKebabCase', () {
+      expect(input.toKebabCase(), toKebabExpect);
+    });
+
+    test('toParamCase', () {
+      expect(input.toParamCase(), toParamExpect);
+    });
+
+    test('toLowerFirstCase', () {
+      expect(input.toLowerFirstCase(), toLowerFirstExpect);
+    });
+
+    test('toNoCase', () {
+      expect(input.toNoCase(), toNoExpect);
+    });
+
+    test('toPascalCase', () {
+      expect(input.toPascalCase(), toPascalExpect);
+    });
+
+    test('toPathCase', () {
+      expect(input.toPathCase(), toPathExpect);
+    });
+
+    test('toSentenceCase', () {
+      expect(input.toSentenceCase(), toSentenceExpect);
+    });
+
+    test('toSnakeCase', () {
+      expect(input.toSnakeCase(), toSnakeExpect);
+    });
+
+    test('toSwapCase', () {
+      expect(input.toSwapCase(), toSwapExpect);
+    });
+
+    test('toTitleCase', () {
+      expect(input.toTitleCase(), toTitleExpect);
+    });
+
+    test('toUpperFirstCase', () {
+      expect(input.toUpperFirstCase(), toUpperFirstExpect);
     });
   }
 }
